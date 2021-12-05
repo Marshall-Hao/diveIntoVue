68a7ac74b29c30d9aeda73d0fb19520a7f54e2be
Marshallccc <Marshallchan666@outlook.com>
2021-12-05 23:59:49 +0800

    LRU with js

---
+++ LRU/script.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.map = new Map();
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (this.map.has(key)) {
+    const val = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key, val);
+    return val;
+  } else {
+    return -1;
+  }
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  const previousVal = this.get(key);
+  if (previousVal === -1) {
+    if (this.map.size === this.capacity) {
+      for (let key of this.map) {
+        this.map.delete(key);
+        break;
+      }
+    }
+  }
+  this.map.set(key, value);
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
